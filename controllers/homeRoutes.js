7175c4f19eec35375c6c6db6228161427466ec77
Popiuy <rymaxpayne@gmail.com>
2023-08-12 13:01:23 -0700

    added index.js for controllers and homeRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const { Album, Movie, Review, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all movies and JOIN with user data
+    const movieData = await Movie.findAll({
+      include: [
+        {
+          model: Review, 
+          attributes: ['content'],
+        },
+        {
+            model: Album, 
+            attributes: ['name', 'href', 'total_plays'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const movies = movieData.map((movies) => movies.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      movies, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/movie/:id', async (req, res) => {
+  try {
+    const movieData = await Movie.findByPk(req.params.id, {
+      include: [
+        {
+          model: Review,
+          attributes: ['content'],
+        },
+        {
+            model: Album, 
+            attributes: ['name', 'href', 'total_plays'],
+        },
+      ],
+    });
+
+    const movie = movieData.get({ plain: true });
+
+    res.render('homepage', {
+      ...movie,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
